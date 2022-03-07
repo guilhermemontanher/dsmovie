6376dbcaaf1d2fe2f50f34baad2c158f7fcf461b
Guilherme Montanher <gui_montanher@live.com>
2022-03-07 20:20:36 -0300

    Bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
