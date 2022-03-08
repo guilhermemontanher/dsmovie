f47c474d497fe0c5bfe891de668ec59d640f736c
Guilherme Montanher <gui_montanher@live.com>
2022-03-07 22:04:43 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,15 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
+import MovieStarts from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+            <MovieCard />
+        </>
+        
     );
 }
 
